486adc2bc2db90bb042722eba531e879c538a848
Mark692 <chiocciolalibero.eu@libero.it>
2019-10-28 10:36:29 +0100

    Service better

--- src/main/java/com/eswproject/lightsouls/Repository/modalitaRepository.java
+++ src/main/java/com/eswproject/lightsouls/Repository/modalitaRepository.java
@@ -5,7 +5,7 @@ import com.eswproject.lightsouls.Domain.Modalita;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface modalitaRepository extends CrudRepository<Modalita, Long> {
-
-    public Modalita findByName(String name);
+public interface modalitaRepository extends CrudRepository<Modalita, Long>
+{
+    //public Modalita findByName(String name);
 }
--- src/main/java/com/eswproject/lightsouls/Service/modalitaService.java
+++ src/main/java/com/eswproject/lightsouls/Service/modalitaService.java
@@ -3,7 +3,10 @@ package com.eswproject.lightsouls.Service;
 import com.eswproject.lightsouls.Domain.Modalita;
 import com.eswproject.lightsouls.Repository.modalitaRepository;
 
-public class modalitaService implements modalitaRepository {
+import java.util.Optional;
+
+public class modalitaService implements modalitaRepository
+{
 
     private static modalitaService single_instance = null;
 
@@ -11,18 +14,81 @@ public class modalitaService implements modalitaRepository {
     public static modalitaService getInstance()
     {
         if (single_instance == null)
+        {
             single_instance = new modalitaService();
+        }
 
         return single_instance;
     }
 
-    @Override
+    //@Override
     public Modalita findByName(String name) {
         return findByName(name);
     }
 
     @Override
-    public void save(Modalita m){
+    public <S extends Modalita> S save(S s)
+    {
+        return null;
+    }
+
+    @Override
+    public <S extends Modalita> Iterable<S> saveAll(Iterable<S> iterable)
+    {
+        return null;
+    }
+
+    @Override
+    public Optional<Modalita> findById(Long aLong)
+    {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong)
+    {
+        return false;
+    }
+
+    @Override
+    public Iterable<Modalita> findAll()
+    {
+        return null;
+    }
+
+    @Override
+    public Iterable<Modalita> findAllById(Iterable<Long> iterable)
+    {
+        return null;
+    }
+
+    @Override
+    public long count()
+    {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong)
+    {
+
+    }
+
+    @Override
+    public void delete(Modalita modalita)
+    {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Modalita> iterable)
+    {
+
+    }
+
+    @Override
+    public void deleteAll()
+    {
 
     }
 }
