5157f008e0b16886b5430f6f773414b0f3676a1e
andy93-16 <andy93-16@hotmail.it>
2019-10-28 14:32:27 +0100

    repository fix

--- src/main/java/com/eswproject/lightsouls/Service/modalitaService.java
+++ src/main/java/com/eswproject/lightsouls/Service/modalitaService.java
@@ -2,93 +2,17 @@ package com.eswproject.lightsouls.Service;
 
 import com.eswproject.lightsouls.Domain.Modalita;
 import com.eswproject.lightsouls.Repository.modalitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-public class modalitaService implements modalitaRepository
+@Service
+public class modalitaService
 {
+    @Autowired
+    private modalitaRepository mR;
 
-    private static modalitaService single_instance = null;
-
-    // static method to create instance of Singleton class
-    public static modalitaService getInstance()
-    {
-        if (single_instance == null)
-        {
-            single_instance = new modalitaService();
-        }
-
-        return single_instance;
-    }
-
-    //@Override
     public Modalita findByName(String name) {
-        return findByName(name);
+        return mR.findByName(name);
     }
 
-    @Override
-    public <S extends Modalita> S save(S s)
-    {
-        return null;
-    }
-
-    @Override
-    public <S extends Modalita> Iterable<S> saveAll(Iterable<S> iterable)
-    {
-        return null;
-    }
-
-    @Override
-    public Optional<Modalita> findById(Long aLong)
-    {
-        return Optional.empty();
-    }
-
-    @Override
-    public boolean existsById(Long aLong)
-    {
-        return false;
-    }
-
-    @Override
-    public Iterable<Modalita> findAll()
-    {
-        return null;
-    }
-
-    @Override
-    public Iterable<Modalita> findAllById(Iterable<Long> iterable)
-    {
-        return null;
-    }
-
-    @Override
-    public long count()
-    {
-        return 0;
-    }
-
-    @Override
-    public void deleteById(Long aLong)
-    {
-
-    }
-
-    @Override
-    public void delete(Modalita modalita)
-    {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Modalita> iterable)
-    {
-
-    }
-
-    @Override
-    public void deleteAll()
-    {
-
-    }
 }
