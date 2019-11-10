bbf004fb68235ce4fed6310f02dde6c449ddea32
andy93-16 <andy93-16@hotmail.it>
2019-11-10 15:00:36 +0100

    finalize game mechanics

--- src/main/java/com/eswproject/lightsouls/Domain/costruttoreModalita.java
+++ src/main/java/com/eswproject/lightsouls/Domain/costruttoreModalita.java
@@ -1,6 +1,14 @@
 package com.eswproject.lightsouls.Domain;
 
+import com.eswproject.lightsouls.Service.modalitaService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public abstract class costruttoreModalita {
+
+	@Autowired
+	private modalitaService service;
+
+	private Modalita m;
 
 	abstract void reset();
 
